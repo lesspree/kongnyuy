1d93a69d3e83359f110edbf6579f51aa589001e3
lesspree <lesspree2e@gmail.com>
2023-05-18 20:49:37 -0500

    my first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+res.send('Hello, World this is my first trial');
+});
+
+const port = 3000;
+app.listen(port, () => {
+console.log(`Server running on port ${port}`);
+});
