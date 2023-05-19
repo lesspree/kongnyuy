ae50b30868d768a795d18933db84e2e36afc36a2
lesspree <133836696+lesspree@users.noreply.github.com>
2023-05-18 21:42:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send('Hello, World this is best zoom');
+res.send('Hello, World this is my first zoom trial');
 });
 
 const port = 3000;
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+console.log(`Server running on port ${port}`);
 });
